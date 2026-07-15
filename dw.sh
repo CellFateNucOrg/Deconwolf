693d77d30667d7630a0ef0cc60cd0b4b1c16ed96
dblu2 <dario_bernasconi@icloud.com>
2026-07-15 11:00:10 +0200

    Add option for tiling

--- dw.sh
+++ dw.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 #SBATCH --job-name=dw
 #SBATCH --output=logs/%x_%j.out
-#SBATCH --time=0-01:00:00
+#SBATCH --time=0-12:00:00
 #SBATCH --cpus-per-task=8
 #SBATCH --mem 64GB
-#SBATCH --gres=gpu:rtx4090:1
+#SBATCH --gres=gpu:rtx6000:1
 
 # Directories with raw images
 img_dirs=(
 
 )
 
-# Configuration
+# Configs for deconvolution
 suffix=nd2
-dw_dir=/path/to/deconwolf
+dw_dir=
 dw_dir=${dw_dir/Volumes/mnt}
 psf_dir=${dw_dir}/psf
 channels=(0 1)
@@ -21,13 +21,13 @@ fluos=(mcherry gfp)
 scope=lipsi
 mag=100
 z_pixel=200
-iterations=50
+tiles=500
 scale=False
-
-# Activate env
-source $HOME/miniforge3/bin/activate dw
+iterations=50
 
 # Run deconvolution
+source $HOME/miniforge3/bin/activate dw
+
 for img_dir in ${img_dirs[@]}; do
 
 	img_dir=${img_dir/Volumes/mnt}
@@ -39,7 +39,8 @@ for img_dir in ${img_dirs[@]}; do
 	-i ${imgs[@]} -p $psf_dir \
 	-c ${channels[@]} -f ${fluos[@]} \
 	-s $scope -m $mag -z $z_pixel \
-	-b $scale -n $iterations
+	-t $tiles -b $scale \
+	-n $iterations
 	
 	mkdir -p ${img_dir}/dw/logs
 	mv ${img_dir}/dw/*log.txt ${img_dir}/dw/logs/
